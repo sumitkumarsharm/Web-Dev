c43bfe558f6b5676020ff7d835893b038f70e7b3
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-07-24 20:25:07 +0530

    chnage in db

--- BACKEND_SHERY/notes/src/db/connectsDB.js
+++ BACKEND_SHERY/notes/src/db/connectsDB.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const connectDB = async () => {
 
-    try{
+    try{ 
          await mongoose.connect(process.env.MONGO_URL)
         .then(() => console.log("MongoDB connected successfully"))
-        
 
+ 
     }
     catch (err) {
         console.error("MongoDB connection error:", err);
