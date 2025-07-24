2a2c0545e4f15156a20405cf052c3bd1fe22226f
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-07-24 20:07:54 +0530

    feat: complete full CRUD functionality using MongoDB and Mongoose

--- BACKEND_SHERY/notes/src/app.js
+++ BACKEND_SHERY/notes/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const noteModel = require('./models/node.model');
 
 const app = express(); 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json()); 
 
 app.post('/notes', async (req, res) => {
         const {title,content} = req.body;      
@@ -11,6 +11,40 @@ app.post('/notes', async (req, res) => {
 });
 
 
+// fetching notes from mongooes 
+app.get('/notes', async (req, res) => {
+    try {   
+        const notes = await noteModel.find({}).sort({ createdAt: -1 });
+        res.status(200).json(notes);
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Failed to fetch notes' });
+    }
+});
+
+
+// update the notes form db
+app.patch('/notes/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const note = await noteModel.findByIdAndUpdate(id, { title, content }, { new: true });
+    if (!note) {    
+        return res.status(404).json({ message: 'Note not found' });
+    }else{
+        res.status(200).json({ message: 'Note updated successfully', note });
+    }
+});
+
+// delete the notes from mongooes
+app.delete('/notes/:id', async (req, res) => {
+    const { id } = req.params;
+    const note = await noteModel.findByIdAndDelete(id)
+    if (!note) {    
+        return res.status(404).json({ message: 'Note not found' });
+    }else{
+        res.status(200).json({ message: 'Note delete successfully', note });
+    }
+});
 
 
 module.exports = app;
