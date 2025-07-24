37be9db08ee6b88927a3a73f9a5e59fda97ad17b
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-07-24 17:33:05 +0530

    feat: complete MongoDB setup and enable full note creation via Mongoose
    Set up Mongoose connection, created note schema, and added POST route to store notes in MongoDB. Verified data insertion successfully.

--- BACKEND_SHERY/notes/src/app.js
+++ BACKEND_SHERY/notes/src/app.js
@@ -11,40 +11,6 @@ app.post('/notes', async (req, res) => {
 });
 
 
-// fetching notes from mongooes
-app.get('/notes', async (req, res) => {
-    try {   
-        const notes = await noteModel.find({}).sort({ createdAt: -1 });
-        res.status(200).json(notes);
-    }
-    catch (err) {
-        res.status(500).json({ error: 'Failed to fetch notes' });
-    }
-});
 
-
-
-// update the notes form db
-app.patch('/notes/:id', async (req, res) => {
-    const { id } = req.params;
-    const { title, content } = req.body;
-    const note = await noteModel.findByIdAndUpdate(id, { title, content }, { new: true });
-    if (!note) {    
-        return res.status(404).json({ message: 'Note not found' });
-    }else{
-        res.status(200).json({ message: 'Note updated successfully', note });
-    }
-});
-
-// delete the notes from mongooes
-app.delete('/notes/:id', async (req, res) => {
-    const { id } = req.params;
-    const note = await noteModel.findByIdAndDelete(id)
-    if (!note) {    
-        return res.status(404).json({ message: 'Note not found' });
-    }else{
-        res.status(200).json({ message: 'Note delete successfully', note });
-    }
-});
 
 module.exports = app;
