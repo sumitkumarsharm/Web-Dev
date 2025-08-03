bd1aaa7ffde9aefebdd8628cda6d179e9eadc98c
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-08-03 22:32:35 +0530

    style: add responsive minimalist design to edit post form

--- MERN_SIGMA/REST_API/index.js
+++ MERN_SIGMA/REST_API/index.js
@@ -1,12 +1,15 @@
 const { log } = require('console');
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+
 app.use(express.json()); // Middleware to parse JSON data 
 app.use(express.urlencoded({ extended: true })); // Middleware to parse JSON and URL-encoded data
+app.use(methodOverride('_method')); // Middleware to support HTTP verbs such as PUT or DELETE in places where the client doesn't support it
 
 // Set up view engine and static files or ejs templates
 app.set('view engine', 'ejs');
@@ -14,7 +17,23 @@ app.set('views', path.join(__dirname, 'views')); // for EJS templates views dire
 app.use(express.static(path.join(__dirname, 'public'))); // for static files in public directory
 
 // constant array for storing Quotes
-let posts = []
+let posts = [
+    {
+        id: uuidv4(), // Generate a unique ID for the post
+        text: "This is the first post.",
+        author: "John Doe"
+    },
+    {
+        id: uuidv4(), // Generate a unique ID for the post
+        text: "This is the second post.",
+        author: "Jane Doe"
+    },
+    {
+        id: uuidv4(), // Generate a unique ID for the post  
+        text: "This is the third post.",
+        author: "John Doe"
+    }
+];
 
 
 
@@ -37,7 +56,7 @@ app.post('/posts', (req, res) => {
     res.redirect('/posts');
 });
 
-// gte by id 
+// get by id
 app.get('/posts/:id', (req, res) => {
     const postId = req.params.id
     const post = posts.find(p => p.id === postId);
@@ -51,8 +70,22 @@ app.get('/posts/:id', (req, res) => {
 
 
 // update by id
-app.get('/update/:id', (req, res) => {
+app.patch('/posts/:id', (req, res) => {
     const postId = req.params.id
+    const newContent = req.body.text;
+    const post = posts.find(p => p.id === postId);
+
+    if (post) {
+        post.text = newContent;
+        res.redirect(`/posts/${postId}`);
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
+
+app.get('/posts/:id/edit', (req, res) => {
+    const postId = req.params.id;
     const post = posts.find(p => p.id === postId);
     if (post) {
         res.render("edit.ejs", { post: post });
